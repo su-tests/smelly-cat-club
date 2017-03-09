cec8360806233d33418343cfceb27477b73529f8
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-03-10 00:00:02 +0300

    dup spec removed

--- spec/controllers/club_controller_spec.rb
+++ spec/controllers/club_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ClubController, type: :controller do
   describe 'GET #catty' do
     it 'returns http success' do
       get :catty
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,8 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
 
   config.before(:suite) do
     FactoryGirl.reload
