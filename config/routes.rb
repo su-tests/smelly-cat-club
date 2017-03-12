5b21c333fcd9f66bded73478637272eaff772d3d
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-03-12 20:26:02 +0300

    restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  get 'registrations/:token', to: 'registrations#new', as: :register_by_token
-  post 'registrations', to: 'registrations#create'
-
   resources :invitations, only: [:index, :new, :create]
+  resources :registrations, only: [:new, :create]
 
   devise_for :users
   root to: 'club#catty'
